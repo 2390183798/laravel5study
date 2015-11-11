1555dead3daf73d5ad3db9da6c12e57ea5765565
dodo2390183798 <2390183798@qq.com>
2015-11-11 09:30:40 +0800

    reuse the view code
    reuse the view code, put the same code into one blade file, and @include
    to the file that wanted to reuse

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -7,23 +7,7 @@
 
     {{--{!! Form::open(['url' => '/articles']) !!}--}}
     {!! Form::open(['url' => '/articles/getForm']) !!}
-    <div class="form-group">
-        {!! Form::label('published_at', 'Publish on:') !!}
-        {!! Form::input('date', 'published_at', date('Y-m-d'), ['class' => 'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::label('title', 'Title:') !!}
-        {!! Form::text('title', null, ['class' => 'form-control']) !!}
-    </div>
-
-    <div class="form-group">
-        {!! Form::label('body', 'Body:') !!}
-        {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
-    </div>
-
-    <div class="form-group">
-        {!! Form::submit('Add Article', ['class' => 'btn btn-primary form-control']) !!}
-    </div>
+    @include('articles._form', ['submitButtonText' => 'Add Article'])
     {!! Form::close() !!}
 
     {{--打印错误信息--}}
--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -6,23 +6,7 @@
     <hr/>
 
     {!! Form::open(['method' => 'PATCH', 'action' => ['ArticlesController@update', $article->id]]) !!}
-    <div class="form-group">
-        {!! Form::label('title', 'Title:') !!}
-        {!! Form::text('title', null, ['class' => 'form-control']) !!}
-    </div>
-
-    <div class="form-group">
-        {!! Form::label('body', 'Body:') !!}
-        {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
-    </div>
-
-    <div class="form-group">
-        {!! Form::label('published_at', 'Publish on:') !!}
-        {!! Form::input('date', 'published_at', date('Y-m-d'), ['class' => 'form-control']) !!}
-    </div>
-    <div class="form-group">
-        {!! Form::submit('Add Article', ['class' => 'btn btn-primary form-control']) !!}
-    </div>
+    @include('articles._form', ['submitButtonText' => 'Update Article'])
     {!! Form::close() !!}
 
     {{--打印错误信息--}}
